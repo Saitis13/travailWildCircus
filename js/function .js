db76785131011fc6fe1e303116eb625a5d05ab38
Saitis13 <45507044+Saitis13@users.noreply.github.com>
2019-07-24 08:09:46 +0200

    Add files via upload

--- js/function .js
+++ js/function .js
@@ -1,30 +1,14 @@
-function squareDigits(num){
-  //may the code be with you
-  alert(num);
-  var newchaine;
-  newchaine="";
-  // decomposition du nombre
-  var chaine = num+"";
-  alert(chaine);
-  for (var i=0 ; i<chaine.length; i++)
-  {
-  // je coupe  la chaine 
-  var lettre=chaine[i];
-  var nombre=parseInt(lettre);
-    // calcul
-  nombre=nombre *nombre;
-  var newelement=String(nombre);  
-  newchaine= newchaine + newelement;
-  alert(newchaine);
-  }
-  return newchaine;
-
-  // sortie du nombre
+function nomfonction (arg){
   
+  
+  return newchaine;
 }
-var num = 9119;
+  // sortie fonction
+  
 
-var result =  squareDigits(num);
+
+
+var result =  nomfonction (arg);
 alert(result);
 result = parseInt(result);
 
